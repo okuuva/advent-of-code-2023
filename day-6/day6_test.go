c2ebb54acf459e5b95161011b229106c3415d09a
Oula Kuuva <oula.kuuva@iki.fi>
2023-12-19 17:45:50 +0200

    feat(day-6): part 2

--- day-6/day6_test.go
+++ day-6/day6_test.go
@@ -7,24 +7,32 @@ import (
 	"github.com/okuuva/advent-of-code-2023/helpers"
 )
 
-var testRaces = []race{
+var part1TestRaces = []race{
 	{7, 9}, {15, 40}, {30, 200},
 }
 var part1Results = []int{4, 8, 9}
 var part1Solution = helpers.Product(part1Results)
 
-var part2Solution = 0
+var part2TestRace = race{71530, 940200}
+var part2Solution = 71503
 
 func TestParseInput(t *testing.T) {
 	s := helpers.NewScanner("testInput.txt")
 	got := parseInput(s)
-	if !reflect.DeepEqual(got, testRaces) {
-		t.Errorf("parseInput: expected '%v', got '%v'", testRaces, got)
+	if !reflect.DeepEqual(got, part1TestRaces) {
+		t.Errorf("parseInput: expected '%v', got '%v'", part1TestRaces, got)
+	}
+}
+
+func TestFixParsedInput(t *testing.T) {
+	got := fixParsedInput(part1TestRaces)
+	if !reflect.DeepEqual(got, part2TestRace) {
+		t.Errorf("parseInputPart2: expected '%v', got '%v'", part2TestRace, got)
 	}
 }
 
 func TestPart1Results(t *testing.T) {
-	got := numbersOfWinningOptions(testRaces)
+	got := numbersOfWinningOptions(part1TestRaces)
 	if !reflect.DeepEqual(got, part1Results) {
 		t.Errorf("part1 results: expected '%v', got '%v'", part1Results, got)
 	}
