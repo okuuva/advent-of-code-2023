83ff3b1cadd0a8a0cf1e11adaed2c1e6b31f11fd
Oula Kuuva <oula.kuuva@iki.fi>
2023-12-20 11:04:04 +0200

    feat(day-1): part 2
    Only fixed the part 2 implementation without doing any further
    refactoring.

--- day-1/part-2/part_2.go
+++ day-1/part-2/part_2.go
@@ -10,44 +10,38 @@ import (
 	"strings"
 )
 
-func getDigits(s string) []string {
-	// FIXME: this does not work with overlapping numbers like oneight
-	// should instead find the index for all numbers in the string and replace first and last spelled
-	// numbers with their digit equivalent
-	pattern := `\d|one|two|three|four|five|six|seven|eight|nine`
-	re := regexp.MustCompile(pattern)
-	return re.FindAllString(s, -1)
+// This wouldn't need a capture group but selecting results is easier when the output is in the same format
+var firstNumberRe = regexp.MustCompile(`(\d|one|two|three|four|five|six|seven|eight|nine)`)
+var lastNumberRe = regexp.MustCompile(`.*(\d|one|two|three|four|five|six|seven|eight|nine).*$`)
+var finders = []*regexp.Regexp{firstNumberRe, lastNumberRe}
+
+var numberToDigit = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
 }
 
 func LineToNumber(s string) int {
-	digits := getDigits(s)
-	if digits == nil {
-		return 0
-	}
 	numberString := strings.Builder{}
-	for _, digit := range [2]string{digits[0], digits[len(digits)-1]} {
-		// thanks copilot for this switch statement
-		// ...though without it I might've spent a little more time to figure a smarter way to do this
-		switch digit {
-		case "one", "1":
-			numberString.WriteString("1")
-		case "two", "2":
-			numberString.WriteString("2")
-		case "three", "3":
-			numberString.WriteString("3")
-		case "four", "4":
-			numberString.WriteString("4")
-		case "five", "5":
-			numberString.WriteString("5")
-		case "six", "6":
-			numberString.WriteString("6")
-		case "seven", "7":
-			numberString.WriteString("7")
-		case "eight", "8":
-			numberString.WriteString("8")
-		case "nine", "9":
-			numberString.WriteString("9")
+	for _, finder := range finders {
+		result := finder.FindStringSubmatch(s)
+		half := ""
+		if result == nil {
+			half = "0"
+		} else {
+			half = result[1]
 		}
+		digit, exists := numberToDigit[half]
+		if !exists {
+			digit = half
+		}
+		numberString.WriteString(digit)
 	}
 	number, _ := strconv.Atoi(numberString.String())
 	return number
